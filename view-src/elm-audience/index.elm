4ecfed7f01ba9bae16531bcdb485bb9ca3c1afb7
OMOTO Kenji <doloopwhile@gmail.com>
2014-11-17 03:01:17 +0900

    tmp

--- view-src/elm-audience/index.elm
+++ view-src/elm-audience/index.elm
@@ -1,4 +1,5 @@
 import Http
+import List
 import Dict
 import Json
 import Signal
@@ -10,18 +11,45 @@ scene : Http.Response String -> Element
 scene resp = case resp of
     Http.Success a ->
       case Json.fromString a of
-        Just j -> (
-          case j of
-            Json.Object d ->
-              case (Dict.get "background_url" d) of
-                Just bg ->
-                  case bg of
-                    Json.String url -> plainText url
-                    _ -> plainText "wrong bg"
-                _ -> plainText "wrong bg"
-            _ -> plainText "wrong bg"
-          )
-        _ -> plainText "wrong JSON"
+        Nothing -> plainText "not json"
+        Just j ->
+          case getPageUrls j of
+            Just urls -> previews urls
+            Nothing -> plainText "json in unexpected type"
     Http.Failure code msg ->
       plainText ((show code) ++ ":" ++ msg)
     _ -> plainText "waiting"
+
+getPageUrls : Json.Value -> Maybe [String]
+getPageUrls j =
+  case j of
+    Json.Object d -> case Dict.get "page_urls" d of
+      Just p -> case p of
+        Json.Array listOfJson -> getStringArray listOfJson
+        _ -> Nothing
+      _ -> Nothing
+    _ -> Nothing
+
+getStringArray : [Json.Value] -> Maybe [String]
+getStringArray vals = realizeAll (List.map getString vals)
+
+getString : Json.Value -> Maybe String
+getString j =
+  case j of
+    Json.String s -> Just s
+    _ -> Nothing
+
+realizeAll : [Maybe a] -> Maybe [a]
+realizeAll arr = List.foldl maybeAppend (Just []) arr
+
+maybeAppend : Maybe a -> Maybe [a] -> Maybe [a]
+maybeAppend mx marr =
+  case (mx, marr) of
+    (Nothing, _) -> Nothing
+    (_, Nothing) -> Nothing
+    ((Just x), (Just arr)) -> Just (arr ++ [x])
+
+previews urls =
+  flow down (map (\url -> plainText url) urls)
+
+
